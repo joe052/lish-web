05c5ec3df845490aca6c1ca29c3ba9eebe6c7cd8
joe052 <joekaranjasenior52@gmail.com>
2023-01-25 23:39:09 +0300

    basic setup

--- web/src/app/layout/layout.module.ts
+++ web/src/app/layout/layout.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LayoutRoutingModule } from './layout-routing.module';
+import { RouterModule } from '@angular/router';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    WrapperComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [CommonModule, RouterModule, LayoutRoutingModule],
 })
-export class LayoutModule { }
+export class LayoutModule {}
