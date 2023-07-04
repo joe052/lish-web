e433d43fb10fd516de3fe41ac136203850d1843a
joe052 <joekaranjasenior52@gmail.com>
2023-07-04 18:55:36 +0300

    message

--- web/src/app/layout/components/pages/about-us/about-us.component.ts
+++ web/src/app/layout/components/pages/about-us/about-us.component.ts
@@ -5,22 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
-export class AboutUsComponent {
-  /**scrolldown immediately after adding new project */
-  scrollDown() {
-    // //ensuring intervals only run once
-    // if (this.projectService.getAddStatus() === true) {
-    //   const setInterval_ID = window.setInterval(() => {
-    //     this.projDiv = document.getElementById('projects');
-    //     this.projDiv.scrollTop = this.projDiv?.scrollHeight;
-    //   }, 100);
-    //   //stopping interval above after sometime
-    //   window.setTimeout(() => {
-    //     window.clearInterval(setInterval_ID);
-    //   }, 500);
-    // }
-    // //unsetting the condition
-    // this.projectService.setAddStatus(false);
-    // //console.log(this.projectService.getAddStatus());
-  }
-}
+export class AboutUsComponent {}
--- web/src/app/services/scroll.service.ts
+++ web/src/app/services/scroll.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScrollService {
+  constructor() {}
 
-  constructor() { }
+  /**method to scroll down */
+  scrollDown() {
+    //ensuring intervals only run once
+    const setInterval_ID = window.setInterval(() => {
+      const projDiv = document.getElementById('projects');
+      // projDiv!.scrollTop = projDiv?.scrollHeight;
+    }, 100);
+    //stopping interval above after sometime
+    window.setTimeout(() => {
+      window.clearInterval(setInterval_ID);
+    }, 500);
+  }
 }
