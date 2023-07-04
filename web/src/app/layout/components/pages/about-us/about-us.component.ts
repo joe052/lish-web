6267ee5a080ed149b3d3e5a6606cd91d878ad152
joe052 <joekaranjasenior52@gmail.com>
2023-07-04 06:26:13 +0300

    message

--- web/src/app/layout/components/pages/about-us/about-us.component.ts
+++ web/src/app/layout/components/pages/about-us/about-us.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent {
-
+  /**scrolldown immediately after adding new project */
+  scrollDown() {
+    // //ensuring intervals only run once
+    // if (this.projectService.getAddStatus() === true) {
+    //   const setInterval_ID = window.setInterval(() => {
+    //     this.projDiv = document.getElementById('projects');
+    //     this.projDiv.scrollTop = this.projDiv?.scrollHeight;
+    //   }, 100);
+    //   //stopping interval above after sometime
+    //   window.setTimeout(() => {
+    //     window.clearInterval(setInterval_ID);
+    //   }, 500);
+    // }
+    // //unsetting the condition
+    // this.projectService.setAddStatus(false);
+    // //console.log(this.projectService.getAddStatus());
+  }
 }
