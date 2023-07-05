9b23f3596fcf0864a7c8ef2fdf2dee45b4ebdba2
joe052 <joekaranjasenior52@gmail.com>
2023-07-05 11:06:42 +0300

    message

--- web/src/app/layout/components/pages/about-us/about-us.component.ts
+++ web/src/app/layout/components/pages/about-us/about-us.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
-export class AboutUsComponent {}
+export class AboutUsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
