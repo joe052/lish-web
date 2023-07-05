4f1c23e65e1c03adaa7db089f991026839fc6f54
joe052 <joekaranjasenior52@gmail.com>
2023-07-05 13:14:07 +0300

    message

--- web/src/app/layout/components/pages/about-us/about-us.component.ts
+++ web/src/app/layout/components/pages/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { ScrollService } from 'src/app/services/scroll.service';
 
 @Component({
   selector: 'app-about-us',
@@ -6,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
-  constructor() {}
+  mainDiv: any;
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  /**method to scroll down */
+  scrollDown() {
+    //ensuring intervals only run once
+    const setInterval_ID = window.setInterval(() => {
+      this.mainDiv = document.getElementById('main');
+      this.mainDiv.scrollTop = this.mainDiv?.scrollHeight;
+    }, 100);
+    //stopping interval above after sometime
+    window.setTimeout(() => {
+      window.clearInterval(setInterval_ID);
+    }, 500);
+  }
 }
