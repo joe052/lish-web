9748ecc3293a35fa9c9c37238797de076b2da1f0
Wambuku <julietwambuku361@gmail.com>
2023-09-27 08:32:01 +0300

    Added Animation and links

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrainingComponent } from './layout/components/pages/training/training.component'; // Import your training component
+import { BlogComponent } from './layout/components/pages/blog/blog.component'; //import your blog component
+import { PartnershipComponent } from './layout/components/pages/partnership/partnership.component'; //import your partnership component
+
 
 const routes: Routes = [
   {
@@ -7,10 +11,42 @@ const routes: Routes = [
     loadChildren: () =>
       import('./layout/layout.module').then((m) => m.CustomLayoutModule),
   },
+  {
+    path: 'training',
+    component: TrainingComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'partnership',
+    component: PartnershipComponent,
+  },
 ];
+// const routes: Routes = [
+//   ... other routes
+//   { path: 'training', component: TrainingComponent }, // Define the training route
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+// app-routing.module.ts
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { TrainingComponent } from './training/training.component'; // Import your training component
+
+// const routes: Routes = [
+//   // ... other routes
+//   { path: 'training', component: TrainingComponent }, // Define the training route
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
