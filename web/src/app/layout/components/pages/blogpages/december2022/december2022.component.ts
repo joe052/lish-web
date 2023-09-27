0d3e0515176484e2f8367d3f6f617160e8b5325e
Flasky06 <bonnienjuguna106@gmail.com>
2023-09-27 19:37:16 +0300

    added blog

--- web/src/app/layout/components/pages/blogpages/december2022/december2022.component.ts
+++ web/src/app/layout/components/pages/blogpages/december2022/december2022.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-december2022',
   templateUrl: './december2022.component.html',
-  styleUrls: ['./december2022.component.scss']
+  styleUrls: ['./december2022.component.scss'],
 })
-export class December2022Component {
-
-}
+export class December2022Component {}
