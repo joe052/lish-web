c35b8c9d09328de26242a9287c91699b65126893
meccah <georgekimiti79@gmail.com>
2024-09-21 15:21:19 +0300

    message

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrainingComponent } from './layout/components/pages/training/training.component'; // Import your training component
+// import { AboutUsComponent } from '.layout/components/pages/about-us/about-us.component';// Import your about-us component
 import { BlogComponent } from './layout/components/pages/blog/blog.component'; //import your blog component
 import { PartnershipComponent } from './layout/components/pages/partnership/partnership.component'; //import your partnership component
 import { CommonModule } from '@angular/common';
@@ -14,7 +15,11 @@ const routes: Routes = [
   {
     path: 'training',
     component: TrainingComponent,
-  },
+  }, 
+  // {
+  //   path: 'about_us',
+  //   component: AboutUsComponent,
+  // },
   {
     path: 'blog',
     component: BlogComponent,
